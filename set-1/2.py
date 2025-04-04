437c7fd49750d3f4f597683e37626ebe448eca70
argon <argon@kali.kali>
2025-04-04 18:10:12 +0530

    initial commit

---
+++ set-1/2.py
@@ -0,0 +1,5 @@
+def table(x):
+    for i in range(1,10):
+        print(x, "*" ,i, "=", x*i)
+
+table(5)
---
+++ set-1/4.py
@@ -0,0 +1,4 @@
+# this is giving the contents given in the directory
+
+import os
+os.listdir()
